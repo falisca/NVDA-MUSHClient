338591d0c534a03cffc3497919f280529db4295a
falisca <lfalisca@gmail.com>
2022-01-22 19:32:42 +0100

    ersion 0.2
    added focus event for activate flat review automatically and minimal description for keys.

--- addon/appModules/mushclient.py
+++ addon/appModules/mushclient.py
@@ -1,5 +1,6 @@
 # mushclient App Module for NVDA
-# Developer 
+## Developer Falisca <lfalisca, {at} google__Mail, DOT, com>
+## unlicense
 
 import api
 import review
@@ -15,52 +16,58 @@ from globalCommands import commands, SCRCAT_TEXTREVIEW
 
 class AppModule(appModuleHandler.AppModule):
 
+    # force the cursor to flat review when the client is focused
+    def event_gainFocus(self, obj, nextHandler):
+        self.moveToFlatReviewAtObjectPosition("")
+        nextHandler()
+
     ##@@## number keys
+    # define  all key shortcuts, hardcoded
     #category=inputCore.SCRCAT_MISC,
     @script(
-        description=_("test"),
+        description=_("speak the last line received"),
         gesture="kb:control+1")
     def script_review_bottom(self, gesture):
         commands.script_review_bottom(gesture)
 
     #category=inputCore.SCRCAT_MISC,
     @script(
-        description=_("test"),
+        description=_("speak the first line printed in the window"),
         gesture="kb:control+2")
     def script_review_top(self, gesture):
         commands.script_review_top(gesture)
 
     #category=inputCore.SCRCAT_MISC,
     @script(
-        description=_("test"),
+        description=_("read the start of the current line in the window"),
         gesture="kb:control+3")
     def script_review_startOfLine(self, gesture):
         commands.script_review_startOfLine(gesture)
 
     #category=inputCore.SCRCAT_MISC,
     @script(
-        description=_("test"),
+        description=_("review the end of current line"),
         gesture="kb:control+4")
     def script_review_endOfLine(self, gesture):
         commands.script_review_endOfLine(gesture)
 
     #category=inputCore.SCRCAT_MISC,
     @script(
-        description=_("test"),
+        description=_("speak the current line"),
         gesture="kb:control+shift+1")
     def script_review_currentLine(self, gesture):
         commands.script_review_currentLine(gesture)
 
     #category=inputCore.SCRCAT_MISC,
     @script(
-        description=_("test"),
+        description=_("speak the current word"),
         gesture="kb:control+shift+2")
     def script_review_currentWord(self, gesture):
         commands.script_review_currentWord(gesture)
 
     #category=inputCore.SCRCAT_MISC,
     @script(
-        description=_("test"),
+        description=_("speak the current character"),
         gesture="kb:control+shift+3")
     def script_review_currentCharacter(self, gesture):
         commands.script_review_currentCharacter(gesture)
@@ -69,28 +76,28 @@ class AppModule(appModuleHandler.AppModule):
 
     #category=inputCore.SCRCAT_MISC,
     @script(
-        description=_("test"),
+        description=_("speak the previous line"),
         gesture="kb:alt+UpArrow")
     def script_review_previousLine(self, gesture):
         commands.script_review_previousLine(gesture)
 
     #category=inputCore.SCRCAT_MISC,
     @script(
-        description=_("test"),
+        description=_("speak the next line"),
         gesture="kb:alt+DownArrow")
     def script_review_nextLine(self, gesture):
         commands.script_review_nextLine(gesture)
 
     #category=inputCore.SCRCAT_MISC,
     @script(
-        description=_("test"),
+        description=_("speak the previous word"),
         gesture="kb:alt+LeftArrow")
     def script_review_previousWord(self, gesture):
         commands.script_review_previousWord(gesture)
 
     #category=inputCore.SCRCAT_MISC,
     @script(
-        description=_("test"),
+        description=_("speak the next word"),
         gesture="kb:alt+RightArrow")
     def script_review_nextWord(self, gesture):
         commands.script_review_nextWord(gesture)
@@ -99,14 +106,14 @@ class AppModule(appModuleHandler.AppModule):
 
     #category=inputCore.SCRCAT_MISC,
     @script(
-        description=_("test"),
+        description=_("speak the previous character"),
         gesture="kb:windows+alt+LeftArrow")
     def script_review_previousCharacter(self, gesture):
         commands.script_review_previousCharacter(gesture)
 
     #category=inputCore.SCRCAT_MISC,
     @script(
-        description=_("test"),
+        description=_("speak the next character"),
         gesture="kb:windows+alt+RightArrow")
     def script_review_nextCharacter(self, gesture):
         commands.script_review_nextCharacter(gesture)
@@ -115,27 +122,27 @@ class AppModule(appModuleHandler.AppModule):
 
     #category=inputCore.SCRCAT_MISC,
     @script(
-        description=_("test"),
+        description=_("speak all the text in the window"),
         gesture="kb:control+L")
     def script_review_sayAll(self, gesture):
         commands.script_review_sayAll(gesture)
 
     #category=inputCore.SCRCAT_MISC,
     @script(
-        description=_("test"),
+        description=_("set the mark for coping a text"),
         gesture="kb:control+alt+space")
     def script_review_markStartForCopy(self, gesture):
         commands.script_review_markStartForCopy(gesture)
     #category=inputCore.SCRCAT_MISC,
     @script(
-        description=_("test"),
+        description=_("copy the selected text"),
         gesture="kb:control+shift+space")
     def script_review_copy(self, gesture):
         commands.script_review_copy(gesture)
 
     #category=inputCore.SCRCAT_MISC,
     @script(
-        description=_("test"),
+        description=_("force the flat review mode"),
         gesture="kb:control+A")
     def script_navigatorObject_moveToFlatReviewAtObjectPosition(self, gesture):
         self.moveToFlatReviewAtObjectPosition(gesture)
--- buildVars.py
+++ buildVars.py
@@ -25,7 +25,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description": _("""Quick and easy shortcuts management for MUSHClient"""),
 	# version
-	"addon_version": "0.1",
+	"addon_version": "0.2",
 	# Author(s)
 	"addon_author": "Falisca <lfalisca@gmail.com>",
 	# URL for the add-on documentation support
